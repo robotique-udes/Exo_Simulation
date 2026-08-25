506f83196b9c44e4d37070530fbb0e397eea11d7
SavariaS <sammysavaria@gmail.com>
2026-08-25 12:21:25 -0400

    feat: Declare ModelBuilder class

---
+++ include/ModelBuilder.hpp
@@ -0,0 +1,64 @@
+/**
+ * @file ModelBuilder.hpp
+ * @brief Declaration of the ModelBuilder class
+ * @details The ModelBuilder takes a base model and has functions for adding
+ *          the exoskeleton, a muscle metabolic probe, actuators and IMUs
+ *
+ * @author Samuel Savaria
+ * @date 2026-08-25
+*/
+#ifndef BIOGENIUS_SIMULATION_MODEL_BUILDER_HPP
+#define BIOGENIUS_SIMULATION_MODEL_BUILDER_HPP
+
+#include <OpenSim/OpenSim.h>
+#include <string>
+
+/**
+ * @brief ModelBuilder takes a base model and has functions for adding the
+ *        exoskeleton, a muscle metabolic probe, actuators and IMUs
+ */
+class ModelBuilder
+{
+public:
+	/**
+	 * @brief Initialize the model from a base model
+	 * @param filename The path to the base model
+	 */
+	ModelBuilder(const std::string& filename);
+
+	/**
+	 * @brief Save the current state of the model to a XML file
+	 * @param filename The path to the file to create
+	 */
+	void print(const std::string& filename);
+
+	/**
+	 * @brief Show the model in the visualizer
+	 */
+	void visualize();
+
+	/**
+	 * @brief Add a muscle metabolic probe that tracks each muscle in the model
+	 */
+	void addMuscleMetabolicProbe();
+
+	/**
+	 * @brief Add the exoskeleton as a separate body
+	 */
+	void addExoskeleton();
+
+	/**
+	 * @brief Add actuators to the hip and knee joints to simulate motors
+	 */
+	void addActuators();
+
+	/**
+	 * @brief Add IMUs
+	 */
+	void addIMU();
+
+private:
+	OpenSim::Model m_model; //< The model to modify
+};
+
+#endif
---
+++ src/ModelBuilder.cpp
@@ -0,0 +1,43 @@
+/**
+ * @file ModelBuilder.cpp
+ * @brief Definition of the ModelBuilder class
+ *
+ * @author Samuel Savaria
+ * @date 2026-08-25
+*/
+#include "ModelBuilder.hpp"
+
+ModelBuilder::ModelBuilder(const std::string& filename)
+{
+
+}
+
+void ModelBuilder::print(const std::string& filename)
+{
+
+}
+
+void ModelBuilder::visualize()
+{
+
+}
+
+void ModelBuilder::addMuscleMetabolicProbe()
+{
+
+}
+
+void ModelBuilder::addExoskeleton()
+{
+
+}
+
+void ModelBuilder::addActuators()
+{
+
+}
+
+void ModelBuilder::addIMU()
+{
+
+}
